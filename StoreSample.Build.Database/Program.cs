21588ed1aaf5c02f7f3ce1b1334fadc717b8a520
Anze Vodovnik <anze.vodovnik@microsoft.com>
2016-06-30 14:37:42 +0100

    Added initial SQL script.

--- StoreSample.Build.Database/Program.cs
+++ StoreSample.Build.Database/Program.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DbUp;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace StoreSample.Build.Database
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            
+            // we want to override the configuration setting, in case we get passed an argument
+            // which is what we will do in the build system
+            var defaultConnectionString = args.FirstOrDefault() ?? System.Configuration.ConfigurationManager.AppSettings["connectionString"];
+
+            var upgrader = DeployChanges.To
+                    .SqlDatabase(defaultConnectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .WithTransactionPerScript()
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
---
+++ StoreSample.Build.Database/Scripts/001 - Init.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE dbo.Books
+(
+	[IdBook] int NOT NULL PRIMARY KEY IDENTITY,
+	[Title] nvarchar(MAX) NOT NULL,
+	[Author] nvarchar(255) NOT NULL,
+	[Description] nvarchar(MAX) NULL,
+	[Price] money NOT NULL
+)
+
+GO
+
+CREATE TABLE dbo.Orders 
+(
+	[IdOrder] int NOT NULL PRIMARY KEY IDENTITY,
+	[BookId] int NOT NULL FOREIGN KEY REFERENCES dbo.Books (IdBook),
+	[OrderPlacedAtUtc] datetime2 NOT NULL DEFAULT SYSUTCDATETIME(),
+	[Quantity] int NOT NULL DEFAULT 1,
+	[TotalPrice] money NOT NULL,
+	[FirstName] nvarchar(255) NOT NULL,
+	[LastName] nvarchar(255) NOT NULL,
+	[Email] nvarchar(255) NOT NULL,
+	[TelephoneNumber] nvarchar(255) NOT NULL,
+	[HouseNumber] nvarchar(5) NOT NULL,
+	[PostCode] nvarchar(8) NOT NULL,
+	[CreditCardNumber] nvarchar(4) NOT NULL
+)
+
+GO
