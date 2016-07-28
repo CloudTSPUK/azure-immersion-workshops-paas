b4e62c2fa465996cc94622a371bd66e3d97678d0
luked <luked@MININT-H34SKUH>
2016-07-28 11:30:26 +0100

    Fleshed out the SqlBookQueryRepository methods to work against the EF dbContext. Modified the BookSearchResult object to include additional properties.

--- StoreSample.Data/Models/BookSearchResult.cs
+++ StoreSample.Data/Models/BookSearchResult.cs
@@ -2,9 +2,22 @@
 
 namespace StoreSample.Data
 {
-    public class BookQueryResult 
+    public class BookQueryResult
     {
+        public BookQueryResult()
+        {
+            this.Result = new List<Book>();
+        }
+
         public int TotalCount { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return this.Result.Count;
+            }
+        }
 
         public List<Book> Result { get; set; }
     }
--- StoreSample.Data/Repositories/SqlBookQueryRepository.cs
+++ StoreSample.Data/Repositories/SqlBookQueryRepository.cs
@@ -1,30 +1,57 @@
-﻿using StoreSample.Data.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace StoreSample.Data.Repositories
+﻿namespace StoreSample.Data.Repositories
 {
+    using StoreSample.Data.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class SqlBookQueryRepository : IBookQueryRepository, IBookSearchRepository
-    {     
+    {
+        private StoreSampleDbContext storeSampleDbContext;
+
         public SqlBookQueryRepository(StoreSampleDbContext storeSampleDbContext)
         {
-            
+            this.storeSampleDbContext = storeSampleDbContext;
         }
 
         public IList<Book> GetAllBooks()
         {
-            throw new NotImplementedException();
+            this.CheckDbContext();
+
+            return this.storeSampleDbContext.Books.ToList();
         }
 
         public Book GetBookById(int bookId)
         {
-            throw new NotImplementedException();
+            this.CheckDbContext();
+
+            return this.storeSampleDbContext.Books.SingleOrDefault(b => b.IdBook == bookId);
         }
 
         public BookQueryResult QueryBooks(BookSearchQuery bookSearchQuery)
         {
-            throw new NotImplementedException();
+            this.CheckDbContext();
+
+            string searchTerm = bookSearchQuery.SearchTerm.ToLowerInvariant();
+
+            List<Book> queryResults = this.storeSampleDbContext.Books.Where(book => (book.Author ?? string.Empty).ToLowerInvariant().Contains(searchTerm)
+                                                                            || (book.Description ?? string.Empty).ToLowerInvariant().Contains(searchTerm)
+                                                                            || (book.Title ?? string.Empty).ToLowerInvariant().Contains(searchTerm))
+                                                                            .ToList();
+
+            return new BookQueryResult()
+            {
+                Result = queryResults,
+                TotalCount = this.storeSampleDbContext.Books.Count()
+            };
+        }
+
+        private void CheckDbContext()
+        {
+            if (this.storeSampleDbContext == null)
+            {
+                throw new NullReferenceException("The book database context connection is null. Cannot query the book database.");
+            }
         }
     }
 }
