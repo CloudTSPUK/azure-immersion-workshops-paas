9ea359e67f23a1c8da8ec1c896b02f30ba3fafc0
luked <luked@MININT-H34SKUH>
2016-08-04 12:48:14 +0100

    Documented IStoreSampleDataSource and made small refactoring to the concrete implementation of the Store Sample Data Source.

--- StoreSample.Data/SqlStoreSampleDataSource.cs
+++ StoreSample.Data/SqlStoreSampleDataSource.cs
@@ -5,6 +5,10 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Provides the basic implementation of the <see cref="IStoreSampleDataSource"/>. The implementation
+    /// is specific to SQL Server and leverages Entity Framework. Therefore has a dependency to the underlying context.
+    /// </summary>
     public class SqlStoreSampleDataSource : IStoreSampleDataSource
     {
         private IList<Book> books;
@@ -15,9 +19,6 @@
         public SqlStoreSampleDataSource()
         {
             this.storeSampleDbContext = new StoreSampleDbContext();
-
-            this.books = storeSampleDbContext.Books.ToList();
-            this.orders = storeSampleDbContext.Orders.ToList();
         }
 
         public IList<Book> Books
@@ -55,8 +56,6 @@
             Guard.NotNull(order, "The provided order was null. Cannot create a new order in the order database from a null order.");
 
             Order newOrder = this.storeSampleDbContext.Orders.Add(order);
-
-            this.SyncOrderCollectionWithDatabase();
 
             return newOrder;
         }
