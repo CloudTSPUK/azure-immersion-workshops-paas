2d2a46334896759d3e64377d2366f41d5dd6c026
Anze Vodovnik <anze.vodovnik@microsoft.com>
2016-09-29 11:58:24 +0100

    Buy page
    Added the buy page, generated view model, linked the views together.
    Updated the fake book model to avoid copyright issues.

--- StoreSample.Web/Controllers/BooksController.cs
+++ StoreSample.Web/Controllers/BooksController.cs
@@ -1,27 +1,93 @@
 ﻿using StoreSample.Data;
+using StoreSample.Web.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace StoreSample.Web.Controllers
 {
     public class BooksController : Controller
     {
-        // GET: Books/Details
-        public ActionResult Details(int id)
+        /// <summary>
+        /// The methods gets the book by id from the data store.
+        /// </summary>
+        /// <remarks>This method only exists because both methods in the controller
+        /// actually need the same one. Typically, this could have been done by introducing
+        /// a service class, but because we wanted to minimize the complexity of the application,
+        /// this will do just fine.</remarks>
+        /// <param name="id">The id of the book to retrieve.</param>
+        /// <returns>Returns a <see cref="Book"/> object, if found, or null if not.</returns>
+        private Book GetBookFromStore(int id)
         {
             var book = new Book()
             {
-                Author = "J.K. Rowling",
-                Title = "Harry Potter: Something something",
+                Author = "R. Andom",
+                Title = "A Short Road To Longevity",
                 Price = 15m,
-                Description = "A very nice book about a wizard.",
+                Description = "A very nice book about a way to go quick, slowly.",
                 IdBook = 1
             };
 
+            return book;
+        }
+
+        /// <summary>
+        /// The action reaches out to the database, to get the proper book, 
+        /// and pass the model to the view.
+        /// </summary>
+        /// <param name="id">The id of the book to show.</param>
+        /// <returns>Returns a view result, with the model of the book.</returns>
+        public ActionResult Details(int id)
+        {
+            var book = GetBookFromStore(id);
             return View(book);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            // NOTE: since we are pretty certain the user came to this page from the Details
+            //       action, we know this could be greatly improved by caching the book. But
+            //       that is premature optimization. 
+            var book = GetBookFromStore(id);
+            var model = new BookPurchaseViewModel()
+            {
+                Quantity = 1, // we obviously default to one 
+                Book = book,
+                // to avoid anybody accidentally inputing their CC number, we prefill it with a GUID
+                CreditCardNumber = Guid.NewGuid().ToString()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Buy(BookPurchaseViewModel model)
+        {
+            // we should be doing validation, and making sure that 
+            // the data is properly entered. But seeing as this is a 
+            // very simple demo, we don't really have to, and it would
+            // again, introduce unnecessary complexity which we can avoid.
+
+            var order = new Order()
+            {
+                BookId = model.Book.IdBook,
+                CreditCardNumber = model.CreditCardNumber,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                HouseNumber = model.HouseNumber,
+                PostCode = model.PostCode,
+                Quantity = model.Quantity,
+                TotalPrice = model.Quantity * model.Book.Price,
+                TelephoneNumber = model.TelephoneNumber,
+                OrderPlacedAtUtc = DateTime.UtcNow
+            };
+
+            // TODO: submit to queue
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ StoreSample.Web/Models/BookPurchaseViewModel.cs
@@ -0,0 +1,47 @@
+﻿using StoreSample.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StoreSample.Web.Models
+{
+    /// <summary>
+    /// This model contains all the details required to view & submit a book
+    /// purchase order.
+    /// </summary>
+    public class BookPurchaseViewModel
+    {
+        /// <summary>
+        /// We might need the book when we're displaying confirmation to the user.
+        /// Note however, this will not get submitted back to us, unless we actually
+        /// use hidden forms in the form that displays it.
+        /// </summary>
+        public Book Book { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telephone Number")]
+        public string TelephoneNumber { get; set; }
+
+        [Display(Name = "House number")]
+        public string HouseNumber { get; set; }
+
+        [Display(Name = "Postal code")]
+        public string PostCode { get; set; }
+
+        [Display(Name = "FAKE Credit Card Number")]
+        public string CreditCardNumber { get; set; }
+    }
+}
