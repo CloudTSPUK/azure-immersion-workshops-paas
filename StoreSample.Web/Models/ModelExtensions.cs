98df7f763083d468381fc0f246f0bac2470a612e
Anže Vodovnik <anze@vodovnik.com>
2017-05-10 14:53:35 +0100

    Added an extension method to unify the retrieval of the cover URL.

---
+++ StoreSample.Web/Models/ModelExtensions.cs
@@ -0,0 +1,23 @@
+﻿using StoreSample.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreSample.Web.Models
+{
+    public static class ModelExtensions
+    {
+        /// <summary>
+        /// Returns the url of the image book.
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public static string GetCoverUrl(this Book book)
+        {
+            var u = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            return u.Content(String.Format("~/Content/images/covers/{0}.png", book.IdBook));
+        }
+    }
+}
